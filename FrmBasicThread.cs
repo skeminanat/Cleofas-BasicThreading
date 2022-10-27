82039ed0b4acf94f32d21a0c9be7491d767b7642
IRhupert <116242246+skeminanat@users.noreply.github.com>
2022-10-27 15:08:46 +0800

    Add files via upload

---
+++ FrmBasicThread.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmBasicThread
+{
+    public partial class FrmBasicThread : Form
+    {
+        private Thread ThreadA, ThreadB;
+        
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("-Before starting thread-");
+                ThreadA = new Thread(MyThreadClass.Thread1);
+                ThreadA.Name = "Thread A";
+                ThreadB = new Thread(MyThreadClass.Thread1);
+                ThreadB.Name = "Thread B";
+                ThreadA.Start();
+                ThreadB.Start();
+
+                ThreadA.Join();
+                ThreadB.Join();
+                Console.WriteLine("-End of Thread-");
+
+                label1.Text = "-End Of Thread-";
+
+            }catch(ThreadAbortException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
---
+++ MyThreadClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrmBasicThread
+{
+    class MyThreadClass
+    {
+        
+
+        public static void Thread1()
+        {
+            for (int LoopCount = 0; LoopCount <= 5; LoopCount++) {
+                
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + LoopCount);
+                Thread.Sleep(1500);
+                
+            }
+            
+                
+                
+           
+        }
+    }
+}
